f1856c376144c824612268960a606efb6e6b7eae
Oare Arene <20444781+oahray@users.noreply.github.com>
2026-06-09 13:09:58 +0100

    Fix vitest version

--- client/src/test/setup.ts
+++ client/src/test/setup.ts
@@ -1,6 +1,8 @@
-import { afterEach, vi } from 'vitest'
+import { afterEach, expect, vi } from 'vitest'
 import { cleanup } from '@testing-library/react'
-import '@testing-library/jest-dom/vitest'
+import * as matchers from '@testing-library/jest-dom/matchers'
+
+expect.extend(matchers)
 
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
